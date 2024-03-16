351efd36e564e0b6b2360bc98c50097413b036ed
ThomazHnr <thomazh2000@gmail.com>
2024-03-16 19:01:10 -0300

    Updated the steps for the run( ) method.

--- ProofOfConcept.sol
+++ ProofOfConcept.sol
@@ -25,38 +25,44 @@ contract ProofOfConcept {
 
   // STATE VARIABLES =================
   uint32 public manufacturer_id;
+  uint32 public consumer_id;
+  uint32 public collector_id;
   uint32 public recycler_id;
   uint32 public product_id;
   uint64 public disposed_device_id;
-  uint16 public amount_of_credits;
   uint32 public amount_of_disposed_devices;
   Material[] public product_materials;
 
   // FUNCTIONS =======================
   function run() public {
-    // 1. register a manufacturer : 
-    manufacturer_id = CStakeholders.addStakeholder("MANUFACTURER", 0x248571eb9273144d);
+    // 1. Registration of stakeholders
+    manufacturer_id = CStakeholders.addStakeholder("MANUFACTURER", 0x123456789abcdef0);
+    consumer_id = CStakeholders.addStakeholder("CONSUMER", 0x123456789abcdef0);
+    collector_id = CStakeholders.addStakeholder("COLLECTOR", 0x123456789abcdef0);
+    recycler_id = CStakeholders.addStakeholder("RECYCLING CENTER", 0x123456789abcdef0);
 
-    // 2. register a recycling center : 
-    recycler_id = CStakeholders.addStakeholder("RECYCLING CENTER", 0x8ed911dd9f814a6f);
+    // 2. Manufacturer registers a product
+    product_id = CProducts.addProduct(manufacturer_id, "Smartphone Model X", "SKU-1");
 
-    // 3. register a product : 
-    product_id = CProducts.addProduct(manufacturer_id, "Smartphone Model X", "SKU 1");
+    // 3. Manufacturer registers the raw materials constituting the product
+    CMaterials.addMaterialOf(product_id, false, "Copper", 60);
+    CMaterials.addMaterialOf(product_id, false, "Silver", 15);
 
-    // 4. register a product's materials : 
-    CMaterials.addMaterialOf(product_id, false, "Copper", 10);
+    // Steps 4 to 6 don't have interactions with the DAO's smart contracts
 
-    // 5. announcement of the disposal of the registered product : 
-    disposed_device_id = CDisposals.dispose(0xb1e9e923035f9390, product_id, recycler_id, manufacturer_id, 1);
+    // 7. Recycling center announces the disposal of the residue
+    disposing_operation_id = CDisposals.dispose( 0x123456789abcdef0, collector_id, consumer_id, manufacturer_id, product_id, recycler_id);
 
-    // 6. give credits : 
-    CCredits.giveCreditTo(manufacturer_id, 10);
-    amount_of_credits = CCredits.getCreditOf(manufacturer_id);
+    // 6. Computing credits for involved stakeholders
+    CCredits.giveCreditTo(manufacturer_id, 100);
+    CCredits.giveCreditTo(consumer_id, 100);
+    CCredits.giveCreditTo(collector_id, 100);
+    CCredits.giveCreditTo(recycler_id, 100);
 
-    // 7. query discarded amount of a product : 
+    // 7. Observer queries the amount of discarded residues 
     amount_of_disposed_devices = CDisposals.getQuantityOf(product_id);
 
-    // 8. query product materials : 
+    // 8. Observer queries the constitution of discarded e-waste
     uint32[] memory material_ids = CMaterials.getMaterialsOf(product_id);
     for (uint32 i = 0; i < material_ids.length; i++) {
       (bool is_toxic, string memory name, uint16 weight_mg) = CMaterials.getMaterial(material_ids[i]);
